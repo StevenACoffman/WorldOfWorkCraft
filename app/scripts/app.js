5e70e59a42e622f0e0e26b1b113dd43bbc0a0483
jwschultz <jwschultz@gmail.com>
2015-03-06 09:54:43 -0500

    Fix mapping for log achievement page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,7 +39,7 @@ angular
         templateUrl: 'views/leaderboard.html',
         controller: 'LeaderboardCtrl'
       })
-      .when('/logachievement/:challengeName', {
+      .when('/logachievement/', {
         templateUrl: 'views/logachievement.html',
         controller: 'LogAchievementCtrl'
       })
