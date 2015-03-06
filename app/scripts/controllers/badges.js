b97dd0054cdcd2e8128ff8e9245ce33d3408ef92
jwschultz <jwschultz@gmail.com>
2015-03-06 00:17:36 -0500

    Earned our stripes
    - (Appropriately) hacked together a method of determining whether
    badges and their constituent achievements have been earned by the
    logged-in user

--- app/scripts/controllers/badges.js
+++ app/scripts/controllers/badges.js
@@ -14,26 +14,52 @@ angular.module('worldOfWorkCraftApp')
     $http.get('http://localhost:8080/worldofworkcraft/badges')
       .success(function(data) {
         $scope.allBadges = data;
+
+        if (UserData.username) {
+          getUserBadges($scope, $http, UserData);
+        }
+
       })
       .error(function(data, status) {
         console.error('Failed to fetch data for all badges: ' + status);
       });
 
-    if (UserData.username) {
-      $http.get('http://localhost:8080/worldofworkcraft/learner/' + UserData.username + '/badges')
-        .success(function(data) {
-          $scope.myBadges = data;
-        })
-        .error(function(data, status) {
-          console.error('Failed to fetch user badge data: ' + status);
-        });
-
-      $http.get('http://localhost:8080/worldofworkcraft/learner/' + UserData.username + '/achievements')
-        .success(function(data) {
-          $scope.myAchievements = data;
-        })
-        .error(function(data, status) {
-          console.error('Failed to fetch user achievement data: ' + status);
-        });
-    }
   }]);
+
+function getUserBadges($scope, $http, UserData) {
+  $http.get('http://localhost:8080/worldofworkcraft/learner/' + UserData.username + '/badges')
+    .success(function(data) {
+      $scope.myBadges = data;
+
+      for (var i = 0; i < $scope.myBadges.length; i++) {
+        for (var j = 0; j < $scope.allBadges.length; j++) {
+          if ($scope.myBadges[i].id === $scope.allBadges[j].id) {
+            $scope.allBadges[j].earned = true;
+          }
+        }
+      }
+
+    })
+    .error(function(data, status) {
+      console.error('Failed to fetch user badge data: ' + status);
+    });
+
+  $http.get('http://localhost:8080/worldofworkcraft/learner/' + UserData.username + '/achievements')
+    .success(function(data) {
+      $scope.myAchievements = data;
+
+      for (var i = 0; i < $scope.myAchievements.length; i++) {
+        for (var j = 0; j < $scope.allBadges.length; j++) {
+          for (var k=0; k < $scope.allBadges[j].achievements.length; k++) {
+            if ($scope.myAchievements[i].id === $scope.allBadges[j].achievements[k].id) {
+              $scope.allBadges[j].achievements[k].earned = true;
+            }
+          }
+        }
+      }
+
+    })
+    .error(function(data, status) {
+      console.error('Failed to fetch user achievement data: ' + status);
+    });
+}
